0b4bcae970a3f4043c829196f4624e8366fcde85
Harish Dhandapani <36785012+harish0619@users.noreply.github.com>
2024-06-26 09:06:27 +0530

    Added a file to use for NP days

---
+++ for_np_days.py
@@ -0,0 +1,11 @@
+# This file is created for days which are not so productive. 
+
+print(list(map(lambda x: x+x, [1,2])))
+
+num1 = [1,2,3]
+
+num2 = [4,5,6]
+
+print(list(map(lambda x,y: x+y, num1, num2)))
+
+list(zip([1,2,3],[4,5,6]))
