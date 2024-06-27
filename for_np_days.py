0a87983ed1918b7caa886b83baab66c28bd70786
Harish Dhandapani <36785012+harish0619@users.noreply.github.com>
2024-06-27 17:41:50 +0530

    Added a closure functions

--- for_np_days.py
+++ for_np_days.py
@@ -9,3 +9,14 @@ num2 = [4,5,6]
 print(list(map(lambda x,y: x+y, num1, num2)))
 
 list(zip([1,2,3],[4,5,6]))
+
+def outer(func):
+  def inner(*args, **kwargs):
+    print(f'calling {func.__name__}')
+    result = func(*args, **kwargs)
+    print('Function called and executed')
+  return inner 
+
+f = outer(lambda a,b: a+b)
+
+f(1,2) 
