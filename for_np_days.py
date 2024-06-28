9d03997b8d1aaff1c0aeafe964bc54a1faf330b6
Harish Dhandapani <36785012+harish0619@users.noreply.github.com>
2024-06-28 10:59:33 +0530

    Added code to raise value exception

--- for_np_days.py
+++ for_np_days.py
@@ -1,5 +1,6 @@
 # This file is created for days which are not so productive. 
 
+# These are zip and map functions 
 print(list(map(lambda x: x+x, [1,2])))
 
 num1 = [1,2,3]
@@ -9,6 +10,9 @@ num2 = [4,5,6]
 print(list(map(lambda x,y: x+y, num1, num2)))
 
 list(zip([1,2,3],[4,5,6]))
+
+
+# This is python closures
 
 def outer(func):
   def inner(*args, **kwargs):
@@ -20,3 +24,12 @@ def outer(func):
 f = outer(lambda a,b: a+b)
 
 f(1,2) 
+
+# How to raise exceptions 
+
+name = 'Jane'
+
+if name.casefold() == 'Jane':
+  print('Right name is passed')
+else:
+  raise ValueError('Please pass the name Jane')
